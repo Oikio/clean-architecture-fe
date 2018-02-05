418377fca883cbe75354152c392efdebb5f8b728
Oikio <oikiostas@gmail.com>
2018-02-05 17:57:03 +0100

    add actions and proper types for enhancer

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,86 @@
 import * as React from 'react'
 import { Component, ComponentClass } from 'react'
 import * as ReactDOM from 'react-dom'
-import { compose } from 'recompose'
+import { compose, withStateHandlers } from 'recompose'
 import { BehaviorSubject } from 'rxjs'
 
 import { mapPropsStream } from './utils/mapPropsStream'
 
-const numbers$ = new BehaviorSubject<number[]>([1])
-numbers$.next([...numbers$.getValue(), 2])
-setTimeout(() => numbers$.next([...numbers$.getValue(), 3]), 2000)
+// State
+const numbers$ = new BehaviorSubject<number[]>([])
 
-interface StateFormStream {
+// Bound Actions
+const clear = () => {
+  numbers$.next([])
+}
+const nextNumber = () => numbers$.next([...numbers$.getValue(), numbers$.getValue().length])
+const setLength = (number: number) => {
+  let i = 0
+  const arr = []
+  while (i < number) {
+    arr.push(i)
+    i++
+  }
+  numbers$.next(arr)
+}
+
+// UseCases
+// TODO: how to work with actions, maybe it makes sense to bind them to streams in useCases?..
+
+// Types for component
+interface NumbersFromState {
   numbers: number[]
 }
 
-const TestView: React.StatelessComponent<any> = props => (
-  <div>{props.numbers}!</div>
+interface State {
+  lengthOfArray: number
+  updateLengthOfArray: (number: number) => void
+}
+
+type Props = NumbersFromState & State
+
+// View
+const TestView: React.StatelessComponent<Props> = props => (
+  <div>
+    <button onClick={clear}>clear</button>
+    <button onClick={nextNumber}>next</button>
+    <button onClick={() => setLength(props.lengthOfArray)}>set</button>
+    <input
+      type="number"
+      onInput={e => props.updateLengthOfArray(parseInt(e.currentTarget.value))}
+      value={props.lengthOfArray}
+    />
+    <div>Numbers: {props.numbers.join(', ')}!</div>
+  </div>
 )
 
-const Test = compose(
-  mapPropsStream<StateFormStream>(props$ =>
+// Enhancer
+const Test = compose<Props, {}>(
+  mapPropsStream<NumbersFromState>(props$ =>
     props$.combineLatest(numbers$, (props, numbers) => {
       return {
         ...props,
         numbers
       }
     })
-  )
+  ),
+  withStateHandlers({
+    lengthOfArray: 1
+  }, {
+      updateLengthOfArray: props => (number: number) => ({
+        lengthOfArray: number
+      })
+    })
 )(TestView)
 
+
+// App
 const AppView: React.StatelessComponent<any> = props => (
   <div>
     <Test />
   </div>
 )
-
 const App = AppView
-
 const root = document.createElement('div')
 document.body.appendChild(root)
-
 ReactDOM.render(<App />, root)
