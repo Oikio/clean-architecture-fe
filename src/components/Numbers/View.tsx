54f66e8b8df362a4b271f3e1c814594bbe732698
Oikio <oikiostas@gmail.com>
2018-03-05 10:02:48 +0100

    make numbers view prettier

--- src/components/Numbers/View.tsx
+++ src/components/Numbers/View.tsx
@@ -12,15 +12,20 @@ export const NumbersView: React.StatelessComponent<Props> = ({
 }) =>
   <div>
 
-    <button onClick={clearNumbersIntent}>clear</button>
-    <button onClick={() => setNumberLengthIntent(lengthOfArray)}>set</button>
-    <input
-      type="number"
-      onChange={e => updateLengthOfArray(parseInt(e.currentTarget.value, 10))}
-      value={lengthOfArray}
-    />
+    <div>
+      <button onClick={clearNumbersIntent}>clear</button>
+      <button onClick={() => setNumberLengthIntent(lengthOfArray)}>set</button>
+      <input
+        type="number"
+        onChange={e => updateLengthOfArray(parseInt(e.currentTarget.value, 10))}
+        value={lengthOfArray}
+      />
+    </div>
 
-    <div>numbers: {numbers.join(', ')}!</div>
-    <div>evenNumbers: {evenNumbers.join(', ')}!</div>
+    <br />
+    <div><b>numbers:</b> {numbers.join(', ')}!</div>
+
+    <br />
+    <div><b>evenNumbers:</b> {evenNumbers.join(', ')}!</div>
 
   </div>
