11bdb1fd034bd878a104f5ff04c0e2b0b22bafca
Oikio <oikiostas@gmail.com>
2018-03-05 17:30:38 +0100

    createLogger (wip)

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,28 +1,10 @@
-import { dispatcher$ } from 'dispatcher'
-import { create } from 'rxjs-spy'
-import { combineLatest } from 'rxjs/operators/combineLatest'
-import { tap } from 'rxjs/operators/tap'
-import { numbers$ } from 'state/numbers'
+import { createLogger } from 'utils/architecture/createLogger'
 
-// TODO: add logging for state and dispatcher
-// console.log(numbers$.operator.tag)
-numbers$
-  .pipe(
-    combineLatest([]),
-    tap((state) => console.log('State: ', state))
-  )
-  .subscribe()
-
-dispatcher$
-  .pipe(tap(({ type, payload }) => console.log(`Intent to ${type} with: `, payload)))
-  .subscribe()
-
-const spy = create()
-spy.log()
+const logger = createLogger()
 
 // HMR
 if (module.hot) {
   module.hot.dispose(() => {
-    spy.teardown()
+    logger.teardown()
   })
 }
---
+++ src/utils/architecture/createLogger.ts
@@ -0,0 +1,25 @@
+import { dispatcher$ } from 'dispatcher'
+import { create } from 'rxjs-spy'
+import { combineLatest } from 'rxjs/operators/combineLatest'
+import { tap } from 'rxjs/operators/tap'
+import { numbers$ } from 'state/numbers'
+
+export const createLogger = () => {
+  // TODO: add logging for state and dispatcher
+  // console.log(numbers$.operator.tag)
+  numbers$
+    .pipe(
+      combineLatest([]),
+      tap((state) => console.log('State: ', state))
+    )
+    .subscribe()
+
+  dispatcher$
+    .pipe(tap(({ type, payload }) => console.log(`Intent to ${type} with: `, payload)))
+    .subscribe()
+
+  const spy = create()
+  spy.log()
+
+  return spy
+}
