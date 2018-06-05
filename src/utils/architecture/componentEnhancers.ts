925fb7fa456c35aa9de274a95014994b0575117b
Oikio <oikiostas@gmail.com>
2018-06-05 20:57:48 +0200

    cleanup

--- src/utils/architecture/componentEnhancers.ts
+++ src/utils/architecture/componentEnhancers.ts
@@ -10,7 +10,6 @@ export const withStateAndIntents = <Props = {}, State = {}, Intents = {}>(
   stream?: (props: Observable<Props>) => Observable<State>,
   intents?: Intents
 ) =>
-  // mapPropsStream(stream as any)
   compose<Props, Props & State & Intents>(
     stream ? mapPropsStream(stream as any) : (i: any) => i,
     intents ? withProps(intents) : (i: any) => i,
