bd4b7dfc29fc487b1130aa9fa0298b5b832f8eef
Oikio <oikiostas@gmail.com>
2018-06-05 23:23:13 +0200

    update types for enhancer

--- src/utils/architecture/componentEnhancers.ts
+++ src/utils/architecture/componentEnhancers.ts
@@ -10,7 +10,7 @@ export const withStateAndIntents = <Props = {}, State = {}, Intents = {}>(
   stream?: (props: Observable<Props>) => Observable<State>,
   intents?: Intents
 ) =>
-  compose<Props, Props & State & Intents>(
+  compose<Props & State & Intents, Props & State & Intents>(
     stream ? mapPropsStream(stream as any) : (i: any) => i,
     intents ? withProps(intents) : (i: any) => i,
   )
