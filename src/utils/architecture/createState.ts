077e9f1f3f77abd9dee78910bcf73144ac78b3f3
Oikio <oikiostas@gmail.com>
2018-06-06 11:18:05 +0200

    fix create state

--- src/utils/architecture/createState.ts
+++ src/utils/architecture/createState.ts
@@ -1,18 +1,15 @@
-import { BehaviorSubject } from 'rxjs'
-import { distinctUntilChanged } from 'rxjs/operators'
-
+import { BehaviorSubject } from 'rxjs';
+import { distinctUntilChanged, distinct } from 'rxjs/operators'
 import { createStateUpdater } from './createStateUpdater'
-
 
 export const createState = <T>(name: string, startWith: T) => {
   const cell = new BehaviorSubject<T>(startWith)
-  const stream = cell.asObservable()
-
-  cell
+  const stream = cell
     .pipe(
       distinctUntilChanged()
     )
-    .subscribe()
+
+  cell.subscribe()
 
   const update = createStateUpdater<T, T>((state, payload) => payload, cell)
   return { cell, stream, update }
