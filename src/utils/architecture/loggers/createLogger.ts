420c8bd01bbee0b5ddb5da9600712d68d37bccc7
Oikio <oikiostas@gmail.com>
2018-12-11 15:47:30 +0100

    add logging breadcrumbs stateChange->updater->useCase

--- src/utils/architecture/loggers/createLogger.ts
+++ src/utils/architecture/loggers/createLogger.ts
@@ -1,5 +1,5 @@
-import { combineLatest, identity, Observable } from 'rxjs'
-import { map, tap, withLatestFrom } from 'rxjs/operators'
+import { combineLatest, identity, merge, Observable, zip } from 'rxjs'
+import { map, tap, withLatestFrom, skip } from 'rxjs/operators'
 
 import { Intent } from '../createIntent'
 
@@ -19,40 +19,53 @@ const devtools = process.env.NODE_ENV === 'development' && (window as any).__RED
 export const createLogger = <T = { [key: string]: Observable<any> }>(
   dispatcher: Observable<Intent>,
   stateUpdaters: Observable<{ name: string, intentName: string, payload?: any }>,
-  stateMap: T,
+  stateMap: T & { [key: string]: Observable<any> },
   ops: { log?: boolean } = {}
 ) => {
 
   const stateNames = Object.keys(stateMap)
-  const stateObservablesArr: Observable<any>[] = stateNames.map(k => (stateMap as any)[k])
+  const stateObservablesArr = stateNames.map(
+    name => (
+      stateMap[name].pipe(
+        map(streamState => ([name, streamState] as [string, any]))
+      )
+    )
+  )
+  const lastChangedState = merge(...stateObservablesArr)
 
-  const state = combineLatest(stateObservablesArr)
+  const state = combineLatest(...stateObservablesArr)
     .pipe(
       map(
-        stateArr => stateNames
-          .map((name, i) => ([name, stateArr[i]]))
-          .reduce((prev: { [key: string]: string }, curr) => {
-            prev[curr[0]] = curr[1]
-            return prev
-          }, {})
+        stateArr => stateArr.reduce((wholeState, [streamName, streamState]) => {
+          wholeState[streamName] = streamState;
+          return wholeState;
+        }, {} as { [key: string]: any })
       )
     )
 
-  const stateLogger = state
+  const stateLogger = zip(
+    // skipping first changes, because state is emitted only after every stateObservable has processed a value (combineLatest)
+    lastChangedState.pipe(skip(stateObservablesArr.length - 1)),
+    state
+  )
     .pipe(
       withLatestFrom(stateUpdaters),
-      tap(([currentState, updater]) => devtools.send(
-        {
-          type: `[updater] ${updater.name} [byIntent] ${updater.intentName}`,
-          payload: updater.payload
-        },
-        currentState
-      )
+      tap(
+        ([[[lastChangedStateName], currentState], updater]) => devtools.send(
+          {
+            type: `[state] ${lastChangedStateName} by ${updater.name} from ${updater.intentName}`,
+            intent: updater.intentName,
+            updater: updater.name,
+            lastChangedState: lastChangedStateName,
+            payload: updater.payload
+          },
+          currentState
+        )
       ),
       ops.log
         ? tap(
-          ([currentState, updater]) => {
-            console.group(`[updater] ${updater.name} [byIntent] ${updater.intentName}`)
+          ([[[lastChangedStateName], currentState], updater]) => {
+            console.group(`[state] ${lastChangedStateName} by ${updater.name} from ${updater.intentName}`)
             console.log(updater.payload)
             console.log(currentState)
             console.groupEnd()
@@ -62,32 +75,6 @@ export const createLogger = <T = { [key: string]: Observable<any> }>(
 
     )
     .subscribe()
-
-  // TODO: return if state logger will be inaccurate
-  // const stateUpdatersLogger = stateUpdaters.pipe(
-  //   withLatestFrom(state),
-  //   tap(
-  //     ([updater, currentState]) =>
-  //       devtools.send(
-  //         {
-  //           type: `[updater] ${updater.name} [byIntent] ${updater.intentName}`,
-  //           payload: updater.payload
-  //         },
-  //         currentState
-  //       )
-  //   ),
-  //   ops.log
-  //     ? tap(
-  //       ([updater, currentState]) => {
-  //         console.group(`[updater] ${updater.name} [byIntent] ${updater.intentName}`)
-  //         console.log(updater)
-  //         console.log(currentState)
-  //         console.groupEnd()
-  //       }
-  //     )
-  //     : identity
-  // )
-  //   .subscribe()
 
   const intentsLogger = dispatcher
     .pipe(
