3397a2a760d2a7f49735874d408266dd5c119da2
Oikio <oikiostas@gmail.com>
2019-09-04 13:47:51 +0200

    enhance wrapLog

--- src/utils/architecture/loggers/logHelpers.ts
+++ src/utils/architecture/loggers/logHelpers.ts
@@ -4,7 +4,7 @@ export const log = process.env.NODE_ENV === 'development'
 
 export const wrapLog =
   process.env.NODE_ENV === 'development'
-    ? <T>(func: T, label?: string): T =>
+    ? <T>(func: T, label: string = (func as any).name): T =>
       ((...args: any[]) => {
         console.group((func as any).name + ' ' + label || '')
         console.log('Arguments = ', args)
