f90656052abe606909a56c2c0ccd29715bc04f2c
Dmitry Loziuk <d.loziuk@sdventures.com>
2021-02-28 05:06:49 +0300

    Rename files

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -4,7 +4,7 @@ import { numbersStream } from '../state/numbers'
 import { numbersWarningStream } from '../state/numbersWarning'
 import { _stateUpdatersStream } from '../utils/architecture/createStateUpdater'
 import { _useCasesStream } from '../utils/architecture/createUseCase'
-import { createLogger } from '../utils/architecture/loggers/createLogger'
+import { createLogger } from '../utils/loggers/create-logger'
 
 
 if (process.env.NODE_ENV === 'development') {
